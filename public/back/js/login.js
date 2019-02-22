7c26e50e41132919eed600d215eac482db5f72bc
cindy-gn <464957784@qq.com>
2019-02-22 11:01:42 +0800

    登录页面完成

--- public/back/js/login.js
+++ public/back/js/login.js
@@ -1,13 +1,13 @@
 
 
-  /*
-   * 1. 进行表单校验配置
-   *    校验要求:
-   *        (1) 用户名不能为空, 长度为2-6位
-   *        (2) 密码不能为空, 长度为6-12位
-   * */
+/*
+ * 1. 进行表单校验配置
+ *    校验要求:
+ *        (1) 用户名不能为空, 长度为2-6位
+ *        (2) 密码不能为空, 长度为6-12位
+ * */
 //这里需要通过一个插件bootstrap-validator  完成 这个插件专门做表单验证的
-$(function(){
+$(function () {
 
     //1.实现表单校验
     $("#form").bootstrapValidator({
@@ -17,33 +17,33 @@ $(function(){
             valid: 'glyphicon glyphicon-ok',
             invalid: 'glyphicon glyphicon-remove',
             validating: 'glyphicon glyphicon-refresh'
-          },
+        },
 
         //1. 字段列表fields 要先在input  里面配置name属性 才能进行表单提交
-        fields:{
+        fields: {
             //用户名
-            username:{
-                validators:{
-                    notEmpty:{
-                        message:"用户名不能为空"
+            username: {
+                validators: {
+                    notEmpty: {
+                        message: "用户名不能为空"
                     },
-                    stringLength:{
-                        min:2,
-                        max:6,
-                        message:"用户名必须在2-6位之间"
+                    stringLength: {
+                        min: 2,
+                        max: 6,
+                        message: "用户名必须在2-6位之间"
                     }
                 }
             },
             //密码
-            password:{
-                validators:{
-                    notEmpty:{
-                        message:"密码不能为空"
+            password: {
+                validators: {
+                    notEmpty: {
+                        message: "密码不能为空"
                     },
-                    stringLength:{
-                        min:6,
-                        max:12,
-                        message:"密码必须是6-12位"
+                    stringLength: {
+                        min: 6,
+                        max: 12,
+                        message: "密码必须是6-12位"
                     }
                 }
             }
@@ -64,32 +64,46 @@ $(function(){
    如果失败会自动拦截
 
     */
-   $("#form").on("success.form.bv",function(e){
-       //组织默认的跳转行为,通过ajax发送请求
-       e.preventDefault();
-       $.ajax({
-           type:"post",
-           //本质上会自动拼接前面的端口号和域名
-           url:"/employee/employeeLogin",
-           //表单序列化就是自动将配置了name属性的input 值进行拼接用于提交
-           data:$("#form").serialize(),
-           dataType:"json",
-           success:function(info){
-               console.log(info);
-               if(info.error==1000){
-                   alert("用户名不存在");
-               }
-               if(info.error==1001){
-                   alert("密码错误");
-               }
-               if(info.success){
-                   location.href="index.html"
-               }
-               
-           }
-       })
-   })
+    $("#form").on("success.form.bv", function (e) {
+        //组织默认的跳转行为,通过ajax发送请求
+        e.preventDefault();
+        $.ajax({
+            type: "post",
+            //本质上会自动拼接前面的端口号和域名
+            url: "/employee/employeeLogin",
+            //表单序列化就是自动将配置了name属性的input 值进行拼接用于提交
+            data: $("#form").serialize(),
+            dataType: "json",
+            success: function (info) {
+                console.log(info);
+                if (info.error == 1000) {
+                    //    alert("用户名不存在");
+                    // updateStatus
+                    // 参数1: 字段名称
+                    // 参数2: 校验状态
+                    // 参数3: 校验规则, 可以设置提示文本
+                    $("#form").data("bootstrapValidator").updateStatus("usrname", "INVALID", "callback")
+                }
+                if (info.error == 1001) {
+                    // alert("密码错误");
+                    $("#form").data("bootstrapValidator").updateStatus("password","INVALID","callback")
+                }
+                if (info.success) {
+                    location.href = "index.html"
+                }
 
+            }
+        })
+    })
+
+
+    //3.重置表单
+    $("[type='reset']").on("click",function(){
+        console.log(1111);
+        
+    $("#form").data("bootstrapValidator").resetForm();
+
+    })
 
 
 
