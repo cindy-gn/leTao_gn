c2bbb63c4cd868f4fd4e4a6334d88cf3ae618d4e
cindy-gn <464957784@qq.com>
2019-02-23 18:57:44 +0800

    index页面的数据可视化完成,其他链接页面初始化完成

--- public/back/js/index.js
+++ public/back/js/index.js
@@ -11,16 +11,20 @@ $(function () {
         },
         tooltip: {},
         legend: {
-            data: ['销量']
+            data: ['人数','销量']
         },
         xAxis: {
             data: ["1月", "2月", "3月", "4月", "5月", "6月"]
         },
         yAxis: {},
         series: [{
+            name: '人数',
+            type: 'bar',
+            data: [520, 220, 306, 410, 310, 270]
+        },{
             name: '销量',
             type: 'bar',
-            data: [5, 20, 36, 10, 10, 20]
+            data: [501, 230, 346, 410, 210, 200]
         }]
     };
 
@@ -30,7 +34,7 @@ $(function () {
     var option2 = {
         title: {
             text: '热门品牌销售',
-            subtext: '纯属虚构',
+            subtext: '2019年2月',
             x: 'center'
         },
         tooltip: {
@@ -40,7 +44,7 @@ $(function () {
         legend: {
             orient: 'vertical',
             left: 'left',
-            data: ['阿迪', '耐克', '老北京', '视频广告', '搜索引擎']
+            data: ['阿迪', '耐克', '老北京', '老奶奶', '新百伦']
         },
         series: [
             {
@@ -52,14 +56,14 @@ $(function () {
                     { value: 335, name: '阿迪' },
                     { value: 310, name: '耐克' },
                     { value: 234, name: '老北京' },
-                    { value: 135, name: '视频广告' },
-                    { value: 1548, name: '搜索引擎' }
+                    { value: 135, name: '老奶奶' },
+                    { value: 1548, name: '新百伦' }
                 ],
                 itemStyle: {
                     emphasis: {
-                        shadowBlur: 10,
-                        shadowOffsetX: 0,
-                        shadowColor: 'rgba(0, 0, 0, 0.5)'
+                        shadowBlur: 20,
+                        shadowOffsetX: 10,
+                        shadowColor: 'yellow'
                     }
                 }
             }
